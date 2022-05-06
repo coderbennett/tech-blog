c57c6b9bdc21bbc07bbc38e7ad05cfcf2a72325f
coderbennett <jbennett@csumb.edu>
2022-05-06 16:41:05 -0700

    add dependencies, connection, middleware and sequelize sync to server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'Oh wow what a secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: newSequelizedStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(routes);
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
