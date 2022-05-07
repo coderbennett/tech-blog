60ad95f803373a731c1ce35e7e3c7b4836907b12
coderbennett <jbennett@csumb.edu>
2022-05-07 14:57:18 -0700

    add api route to get all blogs

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
 const { Blog, User, Comment } = require('../../models');
 
+router.get('/', async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            include: [{ model: User, model: Comment }]
+        });
+
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 router.get('/blog/:id', async (req, res) => {
     try {
         const blogData = await Blog.findByPk(req.params.id, {
