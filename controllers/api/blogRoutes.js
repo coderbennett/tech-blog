fbeab61b980891372f327173846642d28ce47bec
coderbennett <jbennett@csumb.edu>
2022-05-07 14:55:42 -0700

    add api route to get blog by id

---
+++ controllers/api/blogRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { Blog, User, Comment } = require('../../models');
+
+router.get('/blog/:id', async (req, res) => {
+    try {
+        const blogData = await Blog.findByPk(req.params.id, {
+            include: [{ model: User, model: Comment}]
+        });
+
+        if (!blogData) {
+            res.status(404).json({ message: 'No blog with this id!'});
+        }
+
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
