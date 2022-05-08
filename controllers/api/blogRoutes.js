f3a9957a65f91dc841b8367bdeff2410f7401592
coderbennett <jbennett@csumb.edu>
2022-05-07 19:17:00 -0700

    add post, put and delete api routes for blogRoutes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
 
 router.get('/blog/:id', async (req, res) => {
     try {
@@ -27,4 +27,59 @@ router.get('/blog/:id', async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
+router.post('/', async (req, res) => {
+    //creating a new blog post
+    try {
+        const blogData = await Blog.create({
+            name: req.body.name,
+            content: req.body.content,
+            user_id: req.session.user_id
+        });
+
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.put('/blog/:id', async (req, res) => {
+    try {
+        const blogData = await Blog.update({
+            name: req.body.name,
+            content: req.body.content
+        });
+
+        if (!blogData) {
+            res.status(404).json({ message: 'No blog could be found with this id.'});
+            return;
+        }
+
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/blog/:id', async (req, res) => {
+    try {
+        const blogData = await Blog.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            }
+        });
+
+        if (!blogData) {
+            res.status(404).json({ message: 'No blog could be found with this id.'});
+            return;
+        }
+
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
