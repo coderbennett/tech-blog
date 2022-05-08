7991b2d75ae6fdfbd3bf3ca19cc48bdc21836835
coderbennett <jbennett@csumb.edu>
2022-05-07 19:26:54 -0700

    add blog, home and dashboard routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const blogRoutes = require('./blogRoutes');
+const homepageRoutes = require('./homepageRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
 
 router.use('/api', apiRoutes);
+router.use('/blog', blogRoutes);
+router.use('/home', homepageRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
