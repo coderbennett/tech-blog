e7dc3dc3b6e2712af7dd4c6ed118e1e8d06c577f
coderbennett <jbennett@csumb.edu>
2022-05-08 21:42:57 -0700

    add comment route

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { Comment } = require('../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const commentData = await Comment.create({
+            content: req.body.content,
+            blog_id: req.body.blog_id,
+            user_id: req.session.userId
+        });
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+})
