d1c64b9b01da2d5d966b4bca04bec3cdf49e1f8a
joey bennett <joeykb17@gmail.com>
2022-11-02 12:55:41 -0700

    add signup GET route

--- controllers/index.js
+++ controllers/index.js
@@ -25,4 +25,8 @@ router.get('/login', async (req, res) => {
     res.render('login', { loggedIn: req.session.loggedIn});
 });
 
+router.get('/signup', async (req, res) => {
+    res.render('signup', { loggedIn: req.session.loggedIn});
+});
+
 module.exports = router;
